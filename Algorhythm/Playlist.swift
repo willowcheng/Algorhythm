ec5122e9f996e106d1b053fe89c554c40a5d0684
willowcheng <e.wingus@Gmail.com>
2015-05-22 17:28:34 -0400

    Creating music library

---
+++ Algorhythm/Playlist.swift
@@ -0,0 +1,21 @@
+//
+//  Playlist.swift
+//  Algorhythm
+//
+//  Created by Liu Cheng on 2015-05-22.
+//  Copyright (c) 2015 Liu Cheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+}
