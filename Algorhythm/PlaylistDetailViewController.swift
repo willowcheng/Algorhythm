04ee8e7640a6f36c5d2d6d858cfcd2b3d218c51c
willowcheng <e.wingus@Gmail.com>
2015-05-23 00:10:26 -0400

    Displaying playlist information

--- Algorhythm/PlaylistDetailViewController.swift
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -12,10 +12,26 @@ class PlaylistDetailViewController: UIViewController {
 
     var playlist: Playlist?
     
+    @IBOutlet weak var playlistCoverImage: UIImageView!
+    @IBOutlet weak var playlistTitle: UILabel!
+    @IBOutlet weak var playlistDescription: UILabel!
+    
+    @IBOutlet weak var playlistArtist0: UILabel!
+    @IBOutlet weak var playlistArtist1: UILabel!
+    @IBOutlet weak var playlistArtist2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if playlist != nil {
+            playlistCoverImage.image = playlist!.largeIcon
+            playlistCoverImage.backgroundColor = playlist!.backgroundColor
+            playlistTitle.text = playlist!.title
+            playlistDescription.text = playlist!.description
+            
+            playlistArtist0.text = playlist!.artists[0]
+            playlistArtist1.text = playlist!.artists[1]
+            playlistArtist2.text = playlist!.artists[2]
         }
     }
 
@@ -23,6 +39,8 @@ class PlaylistDetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     
 
     /*
